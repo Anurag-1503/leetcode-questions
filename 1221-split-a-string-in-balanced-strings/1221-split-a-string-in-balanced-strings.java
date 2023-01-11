1b8eb455c6fc8b19cc7994ba8ae468304f9c4906
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-01-11 19:54:48 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (99.63%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        
+        int count=0;
+        int r=0,l=0;
+        for(int i=0;i<s.length();i++)
+        {
+            
+            if(s.charAt(i)=='R')
+                r++;
+            if(s.charAt(i)=='L')
+                l++;
+            if(r==l)
+                count++;
+        }
+      return count;  
+    }
+}
