3ee34a1fc32a5fa90012434efcb531a5c6c62ab9
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-01-28 11:49:12 +0530

    Time: 1 ms (57.53%), Space: 41.4 MB (11.02%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,7 +1,9 @@
 class Solution {
     public int nextGreaterElement(int n) {
+         //converting to char Array
         char arr[] = (Integer.toString(n)).toCharArray();
-        
+       
+        //finding the first drop in sequence from right to left
         int i=arr.length-2;
         StringBuilder ans = new StringBuilder();
         while(i>=0 && arr[i] >= arr[i+1])
@@ -10,25 +12,31 @@ class Solution {
         if(i == -1)
             return -1;
         
-        int k = arr.length-1;
         
+        //finding the element which is just greater than the ith indexed-element
+        int k = arr.length-1;
         while(arr[k] <= arr[i])
             k--;
         
+        //swap function to swap elements(array,ith indexed-element,kth indexed element)
         swap(arr,i,k);
         
+        //appending string from 0index to ith index
         for(int j=0;j<=i;j++)
             ans.append(arr[j]);
         
+        //appending reversed string from last index to i+1 index
         for(int j=arr.length-1;j>i;j--)
             ans.append(arr[j]);
         
         long ans_ = Long.parseLong(ans.toString());
         
+        //returning ans if it is valid and fits in 32-bit integer else return -1
         return (ans_ > Integer.MAX_VALUE) ? -1 : (int)ans_;
         
         
     }
+    //swap function
     void swap(char[] arr,int i,int j)
     {
         char temp = arr[j];
