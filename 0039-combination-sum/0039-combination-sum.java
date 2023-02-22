d86a7e28035b718c9daab2f6e73af59723e86519
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-02-22 19:37:28 +0530

    Time: 3 ms (48.00%), Space: 45.9 MB (6.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,26 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        Set<List<Integer>> set
+            = new HashSet<>();
+        findCombinations(0,target,candidates,new ArrayList<>(),set);
         List<List<Integer>> list= new ArrayList<>();
-        findCombinations(0,target,candidates,new ArrayList<>(),list);
+        list = List.copyOf(set);
         return list;
     }
-    private void findCombinations(int index,int target,int[] arr,ArrayList<Integer> ds,List<List<Integer>> list)
+    private void findCombinations(int index,int target,int[] arr,ArrayList<Integer> ds,Set<List<Integer>> set)
     {
     if(index==arr.length)
     {
         if(target==0)
         {
-            list.add(new ArrayList<>(ds));
+            set.add(new ArrayList<>(ds));
         }
         return;
     }
         if(arr[index]<=target)
         {
             ds.add(arr[index]);
-            findCombinations(index,target-arr[index],arr,ds,list);
+            findCombinations(index,target-arr[index],arr,ds,set);
             ds.remove(ds.size()-1);
         }
-     findCombinations(index+1,target,arr,ds,list);
+     findCombinations(index+1,target,arr,ds,set);
     
     }
 }
