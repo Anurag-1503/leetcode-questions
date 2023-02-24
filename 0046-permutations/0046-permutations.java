5e1d4234384f7c782e565ae40201e080b99e7a6a
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-02-24 19:46:53 +0530

    Time: 1 ms (90.11%), Space: 41.1 MB (99.99%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,29 +1,34 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> ans=new ArrayList<>();
-        List<Integer> ds=new ArrayList<>();
-        boolean marked[]= new boolean[nums.length];
-        permutations(nums,ds,ans,marked);
+        List<List<Integer>> ans= new ArrayList<>();
+        func(0,nums,ans);
         return ans;
+        
     }
     
-    private void permutations(int[] nums, List<Integer> ds, List<List<Integer>> ans, boolean marked[])
+    private void func(int index , int nums[] , List<List<Integer>> ans )
     {
-        if(ds.size()==nums.length)
+        if(index==nums.length)
         {
+            List<Integer> ds=new ArrayList<>();
+            for(int i=0;i<nums.length;i++)
+            {
+                ds.add(nums[i]);
+            }
             ans.add(new ArrayList<>(ds));
             return;
         }
-        for(int i=0;i<nums.length;i++)
+        for(int i=index;i<nums.length;i++)
         {
-            if(!marked[i])
-            {
-                marked[i]=true;
-                ds.add(nums[i]);
-                permutations(nums,ds,ans,marked);
-                ds.remove(ds.size()-1);
-                marked[i]=false;
-            }
+            swap(i,index,nums);
+            func(index+1,nums,ans);
+            swap(i,index,nums);
         }
+    }
+    private void swap(int i, int j, int[] nums)
+    {
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
     }
 }
