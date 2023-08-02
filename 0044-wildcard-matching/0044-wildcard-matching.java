7187f21f2b7bf0eda87fa468867dcb1bfbcfcb6a
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-02 11:46:08 +0530

    Time: 16 ms (87.62%), Space: 47.9 MB (5.49%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int n = p.length();
+        int m = s.length();
+        
+        int[][] dp = new int[n][m];
+        for (int[] row: dp) {
+            Arrays.fill(row, -1);
+        }
+        
+        return f(n - 1, m - 1, p, s, dp);
+    }
+    
+    private boolean f(int i, int j, String p, String s, int[][] dp) {
+        
+        // Base Case
+        
+        // if both strings get exhausted
+        if (i < 0 && j < 0) return true;
+        
+        // if one get exhausted and other didn't
+        if (i < 0 && j >= 0) return false;
+        
+        // if p is left, it has to be all '*'
+        if (j < 0 && i >= 0) {
+            for (int k = 0; k <= i; k++) {
+                if (p.charAt(k) != '*') {
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+        
+        if (dp[i][j] != -1) {
+            return dp[i][j] == 1 ? true : false;
+        }
+        
+        // If match found or '?' is found 
+        if (p.charAt(i) == s.charAt(j) || p.charAt(i) == '?') {
+            boolean flag = f(i - 1, j - 1, p, s, dp);
+            dp[i][j] = (flag == true) ? 1 : 0;
+            
+            return flag;
+        }
+        
+        // if it is a '*'
+        if (p.charAt(i) == '*') {
+            boolean flag = f (i - 1, j, p, s, dp) || f (i, j - 1, p, s, dp);
+            dp[i][j] = (flag == true) ? 1 : 0;
+            
+            return flag;
+        }
+        
+        // No match found
+        dp[i][j] = 0;
+        return false;
+    }
+}
