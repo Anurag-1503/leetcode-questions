9d9c084e42c9e38146ddcd9ee075d66a3f470961
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-02 20:49:52 +0530

    Time: 2 ms (17.77%), Space: 44.7 MB (20.99%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,30 +1,29 @@
 class Solution {
     public int maxProfit(int[] prices) {
         
-        int[][] dp = new int[prices.length][2];
-        //2 because there can be two values possible for variable buy
-        for(int[] rows : dp)
-            Arrays.fill(rows,-1);
+        int n = prices.length;
+        int[][] dp = new int[n+1][2];
+       
+        
+        //base cases
+        dp[n][0] = 0;
+        dp[n][1] = 0;
+        
+        
+        for(int index = n-1; index>=0;index--){
+            for(int buy = 0; buy <=1; buy++){
+                int profit = 0;
 
-        return buyorsell(0,0,prices.length,prices,dp);
-        
-    }
-    
-    public int buyorsell(int index, int buy, int n,int[] prices, int[][] dp)
-    {
-        int profit = 0;
-        
-        if(index == n) return 0;
-        
-        if(dp[index][buy] != -1) return dp[index][buy];
-        
-        if(buy == 0)// buy = 0  means you can buy the stock 
-            dp[index][buy] = Math.max(0+buyorsell(index+1,0,n,prices,dp), -prices[index] +                                 buyorsell(index+1,1,n,prices,dp));
-        
-        if(buy == 1)//buy = 1 means you can sell the stock
-            dp[index][buy] = Math.max(0+buyorsell(index+1,1,n,prices,dp), prices[index] +                                   buyorsell(index+1,0,n,prices,dp));
-        
-        return dp[index][buy];
+                if(buy == 0)// buy = 0  means you can buy the stock 
+                    dp[index][buy] = Math.max(0+dp[index+1][0], -prices[index] +dp[index+1][1]);
+
+                if(buy == 1)//buy = 1 means you can sell the stock
+                    dp[index][buy] = Math.max(0+dp[index+1][1], prices[index] + dp[index+1][0]);
+
+            }
+        }
+           
+        return dp[0][0];
             
     }
 }
