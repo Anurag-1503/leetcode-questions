a7645b7d134301f1f55ec1fa84a00a626eb55443
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-04 12:03:03 +0530

    Time: 84 ms (34.63%), Space: 68.9 MB (25.12%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,39 +1,53 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
-        int[][][] dp = new int[prices.length][2][3];
-        for(int[][] rows2 : dp){
-            for(int[] rows1 : rows2)
-            Arrays.fill(rows1,-1);
-        }
-        
-        int cap = 2; //capacity is 2 transaction only
-        return buyAndSell(0,1,cap,prices,dp); // buy = 1 means you are allowed to buy
-        
-    }
-    
-    public int buyAndSell(int ind, int buy, int cap,int[] prices,int[][][] dp){
-        
+            
         int profit = 0;
         int n =prices.length;
-        //if cap becomes 0, that means you can no longer continue doing transactions
-        if(cap == 0) return 0;
-        if(ind == n) return 0;
         
-        if(dp[ind][buy][cap]!= -1) return dp[ind][buy][cap];
+        
+        int[][][] dp = new int[prices.length+1][2+1][3+1];
+        for(int[][] rows2 : dp){
+            for(int[] rows1 : rows2)
+            Arrays.fill(rows1,0);
+         }
+        
+    
+        //base case 1: for cap == 0 , ind and buy can be anything
+        for(int index = n-1;index>=0;index--){
+            for(int buy = 0; buy<=1;buy++){
+                dp[index][buy][0] = 0;
+            }
+        }
+        
+        //base case 2: for index == n, buy and cap can be anything
+        for(int buy = 0; buy <= 1; buy++){
+            for(int cap = 0;cap<=2;cap++){
+                dp[n][buy][cap] = 0;
+            }
+        }
+        
+        for(int ind = n-1; ind>=0;ind--){
+            for(int buy = 0; buy <= 1; buy++){
+                //cap starts from 1 because in base case for cap =0,everything already is calculated
+                for(int cap = 1; cap <= 2;cap++){
+                    if(buy == 1){
+                        dp[ind][buy][cap]= Math.max(-prices[ind] + dp[ind+1][0][cap], 0 + dp[ind+1][1][cap]);
+        }
+        
+         //or you can sell/don't sell
+                    else{
+                        dp[ind][buy][cap]= Math.max(prices[ind] + dp[ind+1][1][cap-1] ,0 + dp[ind+1][0][cap]);
+        }
+                }
+            }
+        }
+      
         
         
         //either you can buy/don't buy
-        if(buy == 1){
-             dp[ind][buy][cap]= Math.max(-prices[ind] + buyAndSell(ind+1,0,cap,prices,dp), 0 + buyAndSell(ind+1,1,cap,prices,dp));
-        }
-        
-        //or you can sell/don't sell
-        else{
-            dp[ind][buy][cap]= Math.max(prices[ind] + buyAndSell(ind+1,1,cap-1,prices,dp),0 + buyAndSell(ind+1,0,cap,prices,dp));
-        }
         
         
-        return  dp[ind][buy][cap];
+        
+        return  dp[0][1][2];
     }
 }
