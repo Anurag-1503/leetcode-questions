79bfbc9d9a244588390d4f44620d9c76478d9a10
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-05 12:10:20 +0530

    Time: 1 ms (99.85%), Space: 43.6 MB (86.04%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -3,20 +3,25 @@ class Solution {
     
         int  n = nums.length;
         int[] arr = new int[n];
-        for(int i = 0; i < n ; i++)
-        {
-            int count = 0;
-            for(int j = 0; j<n ; j++)
-            {
-                if(nums[j] < nums[i] && j != i)
-                {
-                    count++;
-                }
-                arr[i] = count;
-            }
-             
-        }
-        return arr;
+        //101 because "0 <= nums[i] <= 100"
+        int[] table = new int[101];
+        int[] ans = new int[n];
         
+        for(int i = 0 ; i < n ; i++){
+            table[nums[i]]++;
+        }
+        
+        for(int i = 1; i < table.length ; i++){
+            table[i] = table[i] + table[i-1];
+        }
+        
+        for(int i = 0 ; i < n; i++){
+            if(nums[i] == 0)
+                ans[i] = 0;
+            else
+                ans[i] = table[nums[i]-1];
+        }
+        
+        return ans;
     }
 }
