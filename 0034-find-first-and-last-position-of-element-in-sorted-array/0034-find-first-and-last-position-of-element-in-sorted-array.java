6061a88cc1fb35565a04f54d68caf879f3e35af0
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-10 10:14:43 +0530

    Time: 1 ms (16.55%), Space: 44.5 MB (44.89%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,56 +1,23 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int [] result = new int [2];
-        result[0]= findstartingindex(nums , target);
-        result[1]= findendingindex(nums , target);
-        return result;
-    }
-    
-    public int findstartingindex(int [] nums, int target)
-    {
-        int index=-1;
-        int start = 0;
-        int end = nums.length-1;
         
-        while( start <= end)
-        {
-            int midpoint = start + (end - start) /2;
-            if(nums[midpoint] >=  target)
-            {
-                end = midpoint-1;
-            }
-            else
-            {
-                start = midpoint +1;
-            }
-            if(nums[midpoint] == target)
-                index = midpoint;
-            
-        }
-        return index ;
-    }
-    
-    public int findendingindex(int [] nums, int target)
-    {
-        int index=-1;
-        int start = 0;
-        int end = nums.length-1;
+        int n = nums.length;
+        List<Integer> range = new ArrayList<>();
+        int[] res = new int[2];
+        Arrays.fill(res,-1);
         
-        while( start <= end)
-        {
-             int midpoint = start + (end - start) /2;
-            
-            if(nums[midpoint] <=  target )
-            {
-                start = midpoint+1;
+        for(int i = 0 ; i < n ; i++){
+            if(nums[i] == target){
+                range.add(i);
             }
-            else 
-            {
-                end =  midpoint -1;
-            }
-            if(nums[midpoint]== target)
-                index = midpoint;
         }
-        return index;
+        
+        if(range.size() == 0) 
+            return res;
+        
+        res[0] = range.get(0);
+        res[1] = range.get(range.size()-1);
+        
+        return res;
     }
 }
