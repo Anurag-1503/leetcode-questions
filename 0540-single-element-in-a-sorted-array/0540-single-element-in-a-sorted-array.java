69009b2c203f38d075adf602159827e3e10c24d5
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-25 15:14:14 +0530

    Time: 11 ms (5.09%), Space: 53.1 MB (6.44%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,13 +1,18 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int left = 0, right = nums.length-1;
-        while(left < right){
-            int mid = (left + right)/2;
-            if( (mid % 2 == 0 && nums[mid] == nums[mid +1]) || (mid %2 == 1 && nums[mid] == nums[mid - 1]) )
-                left = mid + 1;
-            else
-                right = mid;
+        
+        Map<Integer, Integer> fmap = new HashMap<>();
+        
+        for(int num : nums){
+            fmap.put(num, fmap.getOrDefault(num,0)+1);
         }
-        return nums[left];
-    }   
+        
+        for(Map.Entry<Integer, Integer> entry : fmap.entrySet()){
+            if(entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+        
+        return -1;
+    }
 }
