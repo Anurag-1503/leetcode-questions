4121b1eec7e063ea86238a15e9155944874ccf7a
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2023-08-26 21:38:07 +0530

    Time: 0 ms (100.00%), Space: 50 MB (59.55%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -4,21 +4,31 @@ class Solution {
         int n = nums.length;
         
         if(n == 1) return nums[0];
+        //check for 1st and last element separately as we then start with 2nd and last-1 element
         
-        for(int i = 0 ; i < n ; i++){
-            if(i == 0){
-                if(nums[i] != nums[i+1])
-                    return nums[i];
-            }  
-            else if (i == n-1){
-                if(nums[i] != nums[i-1])
-                    return nums[i];
-            } 
-            else{
-                if(nums[i] != nums[i-1] && nums[i] != nums[i+1])
-                    return nums[i];
-            }
+        if(nums[0] != nums[1]) return nums[0];
+        if(nums[n-1] != nums[n-2]) return nums[n-1];
+        
+        //start binary search with 2nd and last-1 element
+        
+        int start = 1;
+        int end = n-2;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
+                return nums[mid];
+            
+            //check for left half
+            if( (mid%2 == 1) && nums[mid] == nums[mid-1] || (mid%2 == 0) && nums[mid] == nums[mid+1])
+                start = mid + 1;
+            //check for right half
+            else
+                end = mid - 1;
         }
+        
         return -1;
+        
     }
 }
