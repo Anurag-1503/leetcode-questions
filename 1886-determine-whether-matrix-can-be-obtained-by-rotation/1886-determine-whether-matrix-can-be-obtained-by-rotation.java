9504121c58c50de26a52756b812ee007f6443294
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-14 20:27:38 +0530

    Time: 1 ms (54.55%), Space: 41.7 MB (25.99%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.java
@@ -1,50 +1,56 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         
-        int n = mat.length;
-        for(int i = 0; i < 4 ; i++){
-            if(check(mat,target))
-                return true;
-            rotate(mat);
-        }
+        rotateClockwise(mat);
+        if(Arrays.deepEquals(mat,target))
+            return true;
         
-        return false;
+        rotateClockwise(mat);
+        if(Arrays.deepEquals(mat,target))
+            return true;
+        
+        rotateClockwise(mat);
+        if(Arrays.deepEquals(mat,target))
+            return true;
+        
+        rotateClockwise(mat);
+        return Arrays.deepEquals(mat,target);
+        
     }
     
-    public boolean check(int[][] mat, int[][] target){
-        int n = mat.length;
-        for(int i = 0 ; i < n ; i++){
-            for(int j = 0; j < n; j++){
-                if(mat[i][j]!=target[i][j])
-                    return false;
-            }
-        }
-        return true;
+    
+    public void rotateClockwise(int[][] mat){
+        Transpose(mat);
+        reverseCols(mat);
     }
     
-    public void rotate(int[][] mat){
-        int n = mat.length;
-        //transpose of matrix
-        for(int i = 0; i < n ;i++){
-            for(int j = 0 ; j<i;j++){
+    
+    
+    public void Transpose(int[][] mat){
+
+        for(int i = 0 ; i < mat.length ; i++){
+            for(int j = i ; j < mat[0].length ; j++){
                 int temp = mat[i][j];
                 mat[i][j] = mat[j][i];
                 mat[j][i] = temp;
             }
         }
-        
-        //swap cols(start with end then start++ and end-- till they cross each other)
-        
-        int start = 0;
-        int end = n-1;
-        while(start<=end){
-            for(int row = 0;row < n; row++){
-                int temp = mat[row][start];
-                mat[row][start] = mat[row][end];
-                mat[row][end] = temp;
-            }
-            start++;
-            end--;
-        }
     }
+    
+    public void reverseCols(int[][] mat){
+        
+         for(int i = 0 ; i < mat.length ; i++){
+             int leftIndex = 0;
+             int rightIndex = mat[i].length - 1;
+             
+             while(leftIndex < rightIndex){
+                 int temp = mat[i][leftIndex];
+                 mat[i][leftIndex] = mat[i][rightIndex];
+                 mat[i][rightIndex] = temp;
+                 leftIndex++;
+                 rightIndex--;
+             }
+         }
+    }
+    
 }
