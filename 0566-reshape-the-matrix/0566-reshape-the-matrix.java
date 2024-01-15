5d058bb1229a1cadac2ff1954de673cd4879742e
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-15 20:12:48 +0530

    Time: 0 ms (100.00%), Space: 45.7 MB (5.70%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -4,22 +4,33 @@ class Solution {
         int rows = mat.length;
         int cols = mat[0].length;
         
-        if((rows*cols) != (r * c)) return mat;
+        //we have to check if rows*cols == r*c
+        //if not equal , return the original matrix mat
         
-        int[][] arr =new int[r][c];
+        if((rows*cols) != (r*c)) return mat;
         
-        int row_num = 0;
-        int col_num = 0;
-        for(int i = 0 ; i < rows ; i++){
-            for(int j = 0 ; j < cols; j++){
-                arr[row_num][col_num] = mat[i][j];
-                col_num++;
-                if(col_num == c){
-                    col_num = 0;
-                    row_num++;
+        //create new matrix;
+        int res[][] = new int[r][c];
+        //take two pointers for rows and cols to insert
+        int new_rows = 0;
+        int new_cols = 0;
+        
+        //traverse throught original matrix and insert elements in new matrix
+        for(int i = 0; i < rows ; i++){
+            for(int j = 0 ; j < cols ; j++){
+                
+                res[new_rows][new_cols] = mat[i][j];
+                //increment new_cols because we have to insert elements column-wise
+                new_cols++;
+                
+                //check if new_cols reaches till 'c' , if yes then reset new_cols to 0 and increment new_row
+                if(new_cols == c){
+                    new_cols = 0;
+                    new_rows++;
                 }
             }
         }
-        return arr;
+        
+        return res;
     }
 }
