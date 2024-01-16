219daac109337ee27ec7cbfc8d5eded387d5f05f
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-16 13:44:44 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (44.49%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -1,42 +1,48 @@
 class Solution {
     public int[][] generateMatrix(int n) {
         
-        int[][] arr = new int[n][n];
-        int counter = 1;
+        int[][] matrix = new int[n][n];
         
-         int left = 0 , right = n - 1, top = 0 , bottom = n - 1;
+        int rowBegin = 0;
+        int rowEnd = n-1;
+        int colBegin = 0;
+        int colEnd = n-1;
+        int count = 1;
         
-        
-        while(left <= right && top <= bottom){
+        while(rowBegin <= rowEnd && colBegin <= colEnd){
             
-        for(int i = left; i <= right ; i++){
-            arr[top][i] = counter++;
-        }
-        top++;
-        
-        for(int i = top; i <= bottom; i++){
-            arr[i][right] = counter++;
-        }
-        right--;
-        if(top <= bottom){
             
-             for(int i = right; i>= left; i--){
-                arr[bottom][i] = counter++;
+            for(int i = colBegin ; i <= colEnd ; i++){
+                matrix[rowBegin][i] = count;
+                count++;
             }
-            bottom--;   
-            }
-        
-       if(left <= right){
-        for(int i = bottom; i >= top; i--){
-            arr[i][left] = counter++;
-        }
-        left++;
+            rowBegin++;
             
-        }
+            for(int i = rowBegin ; i <= rowEnd ; i++){
+                matrix[i][colEnd] = count;
+                count++;
+            }
+            colEnd--;
+            
+            if(rowBegin <= rowEnd){
+                for(int i = colEnd ; i >= colBegin ; i--){
+                    matrix[rowEnd][i] = count;
+                    count++;
+                }
+                rowEnd--;
+            }
+            
+            if(colBegin <= colEnd){
+                for(int i = rowEnd ; i >= rowBegin ; i--){
+                    matrix[i][colBegin] = count;
+                    count++;
+                }
+                colBegin++;
+            }
+               
         }
         
+        return matrix;
         
-
-        return arr;
     }
 }
