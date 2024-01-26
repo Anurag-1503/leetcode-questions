e6f6e66a3e0ec594450912ef7bb8054c954a4e3b
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-26 12:05:12 +0530

    Time: 5 ms (9.70%), Space: 43.8 MB (8.57%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,14 +1,38 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         
-        for(int i = 0 ; i < arr.length ; i++){
-            for(int j = 0 ; j < arr.length ; j++){
-                if(i != j){
-                    if(arr[i]*2 == arr[j] || arr[j]*2 == arr[i])
-                        return true;
-                }
+         Map<Integer, Integer> map = new HashMap<>();
+
+        // Handle the zero case separately
+        int zeroCount = 0;
+
+        for (int num : arr) {
+            if (num == 0) {
+                zeroCount++;
+            } else {
+                map.put(num, map.getOrDefault(num, 0) + 1);
             }
         }
+
+        if (zeroCount > 1) {
+            return true;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            int doubleValue = arr[i] * 2;
+            int halfValue = arr[i] / 2;
+
+            // Check if doubleValue is present in the map
+            if (map.containsKey(doubleValue) && map.get(doubleValue) > 0) {
+                return true;
+            }
+
+            // Check if halfValue is present in the map
+            if (arr[i] % 2 == 0 && map.containsKey(halfValue) && map.get(halfValue) > 0) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
