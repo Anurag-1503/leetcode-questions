e69cc9a54290afa6970a3bd0a0cb695df1308cc7
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-26 11:54:11 +0530

    Time: 3 ms (37.05%), Space: 42.4 MB (77.21%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,14 +1,13 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
         
-        int n = arr.length;
-        Map<Double, Integer> map = new HashMap<>();
-        
-        for(int i = 0; i < n; i++){
-            if(map.containsKey((double)arr[i]*2) || map.containsKey((double)arr[i]/2)){
-                return true;
+        for(int i = 0 ; i < arr.length ; i++){
+            for(int j = 0 ; j < arr.length ; j++){
+                if(i != j){
+                    if(arr[i]*2 == arr[j] || arr[j]*2 == arr[i])
+                        return true;
+                }
             }
-            map.put((double)arr[i],1);
         }
         return false;
     }
