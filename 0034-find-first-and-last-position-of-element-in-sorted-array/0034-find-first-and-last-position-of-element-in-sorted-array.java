aebaa805edeb4e42185f37bf85414f43e6ad2e29
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-26 13:47:20 +0530

    Time: 0 ms (100.00%), Space: 45.9 MB (36.84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,29 +1,58 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        if(nums.length == 0)
-            return new int[]{-1,-1};
+        int[] result = new int[2];
+        result[0] = findFirstIndex(nums,target);
+        result[1] = findLastIndex(nums,target);
+        return result;
         
+    }
+    
+    public int findFirstIndex(int[] nums , int target){
+        int index = -1;
         
-        int[] res = new int[2];
-        //this loop will get the first occurence of the target element
-        for(int i = 0 ; i < nums.length ; i++){
-            if(nums[i] == target){
-                res[0] = i;
-                break;
-            }
-            res[0] = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] >= target)
+                end = mid - 1;
+            else
+                start = mid + 1;
+            
+            if(nums[mid] == target)
+                index = mid;
+            
         }
-      
-        for(int i = nums.length - 1; i >= 0 ; i--){
-            if(nums[i] == target)
-            {
-                res[1] = i;
-                break;
-            }
-            res[1] = -1;
+        
+        
+        return index;
+    }
+    public int findLastIndex(int[] nums , int target){
+        int index = -1;
+        
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            
+            int mid = start + (end - start)/2;
+            
+            if(nums[mid] <= target)
+                start = mid + 1;
+            else
+                end = mid - 1;
+            
+            if(nums[mid] == target)
+                index = mid;
+            
         }
         
-        return res;
+        
+        return index;
+        
     }
 }
