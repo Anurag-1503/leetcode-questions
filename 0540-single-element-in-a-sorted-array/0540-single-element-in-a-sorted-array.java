51eee99ca3c18667e9cf071b7e36ba4d3ce4a7b6
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-27 19:23:49 +0530

    Time: 3 ms (6.58%), Space: 50.2 MB (53.70%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -1,31 +1,21 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         
-        int n = nums.length;
+        if(nums.length == 1)
+            return nums[0];
         
-        if(n == 1) return nums[0];
-        //check for 1st and last element separately as we then start with 2nd and last-1 element
-        
-        if(nums[0] != nums[1]) return nums[0];
-        if(nums[n-1] != nums[n-2]) return nums[n-1];
-        
-        //start binary search with 2nd and last-1 element
-        
-        int start = 1;
-        int end = n-2;
-        
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        for(int i = 0 ; i < nums.length ; i++){
             
-            if(nums[mid] != nums[mid+1] && nums[mid] != nums[mid-1])
-                return nums[mid];
-            
-            //check for left half
-            if( (mid%2 == 1) && nums[mid] == nums[mid-1] || (mid%2 == 0) && nums[mid] == nums[mid+1])
-                start = mid + 1;
-            //check for right half
-            else
-                end = mid - 1;
+            //1 : if index is 0
+            if(i == 0 && nums[i] != nums[i+1])
+                    return nums[i];
+            //2 : if index is last
+            else if(i == nums.length - 1 && nums[i] != nums[i-1])
+                    return nums[i];
+            //3 : rest elements
+            else if(i > 0 && i < nums.length - 1 && nums[i] != nums[i+1] && nums[i] != nums[i-1])
+                    return nums[i];
+                
         }
         
         return -1;
