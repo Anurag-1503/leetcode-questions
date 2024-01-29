d8db9725400fe49b41a136fcd8d74011757b181d
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-01-29 11:47:30 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (56.66%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,15 +1,27 @@
 class Solution {
     public int findMin(int[] nums) {
         
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int num : nums) {
-            map.put(num, 1);
+        int min = Integer.MAX_VALUE;
+        int start = 0;
+        int end = nums.length - 1;
+        
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            //check if left half is sorted or not
+            if(nums[start] <= nums[mid]){
+                //if its sorted , then take the minimum of sorted half
+                min = Math.min(min , nums[start]);
+                start = mid + 1;
+            }
+            //check if right half is sorted or not
+            if(nums[mid]<= nums[end]){
+                //if it is sorted , take the minimum of it
+                min = Math.min(min , nums[mid]);
+                end = mid - 1;
+            }
         }
-        int minValue = Integer.MAX_VALUE;
-        for (int num : map.keySet()) {
-            minValue = Math.min(minValue, num);
-        }
-        return minValue;
+        
+        return min;
         
     }
 }
