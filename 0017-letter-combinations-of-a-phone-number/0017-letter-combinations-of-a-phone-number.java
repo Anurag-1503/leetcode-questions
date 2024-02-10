9effc172d7b0e78bbb0dfbc29dbddd469265d3bb
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-10 12:05:41 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (85.98%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,45 +1,45 @@
 class Solution {
     
-    private List<String> result = new ArrayList<>();
-    
-    
+    List<String> result = new ArrayList<>();
     public List<String> letterCombinations(String digits) {
         
-        if (digits.length() == 0) {
-            return new ArrayList<>();
-        }
-
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
-
+        if(digits.length() == 0)
+            return result;
+        
+        Map<Character , String> map = new HashMap<>();
+        map.put('2' , "abc");
+        map.put('3' , "def");
+        map.put('4' , "ghi");
+        map.put('5' , "jkl");
+        map.put('6' , "mno");
+        map.put('7' , "pqrs");
+        map.put('8' , "tuv");
+        map.put('9' , "wxyz");
+        
         StringBuilder temp = new StringBuilder();
-
-        solve(0, digits, temp, map);
-
+        Solve(0 , digits , temp , map);
         return result;
+        
     }
-
-    private void solve(int idx, String digits, StringBuilder temp, Map<Character, String> map) {
-        if (idx >= digits.length()) {
+    
+    public void Solve(int index , String digits , StringBuilder temp , Map<Character , String> map){
+        
+        if(index == digits.length()){
             result.add(temp.toString());
             return;
         }
-
-        char ch = digits.charAt(idx);
+        
+        //get the first Character from input string
+        char ch = digits.charAt(index);
+        //get the corresponding letters assosiated with the Character
         String str = map.get(ch);
-
-        for (int i = 0; i < str.length(); i++) {
-            // Do
+        
+        for(int i = 0 ; i < str.length() ; i++){
             temp.append(str.charAt(i));
-            solve(idx + 1, digits, temp, map);
+            Solve(index + 1, digits , temp , map);
             temp.deleteCharAt(temp.length() - 1);
         }
+        
+        
     }
 }
