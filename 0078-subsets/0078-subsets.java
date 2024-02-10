2cc461492a778ac22ab87ea26810cc84d6a403b0
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-10 13:33:52 +0530

    Time: 0 ms (100.00%), Space: 42.6 MB (62.30%) - LeetHub

---
+++ 0078-subsets/0078-subsets.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> ds = new ArrayList<>();
+        helper(0,nums,result,ds);
+        return result;
+    }
+    
+    public void helper(int index , int[] nums ,List<List<Integer>> result ,List<Integer> ds){
+        
+        //base condition
+        if(index >= nums.length)
+        {
+            result.add(new ArrayList<>(ds));
+            return;
+        }
+        
+        ds.add(nums[index]);
+        helper(index + 1, nums , result , ds);
+        ds.remove(ds.size()-1);
+        helper(index + 1 , nums , result ,ds);
+    }
+}
