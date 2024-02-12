7c460e24d036de5760e9e5121d339f868304070d
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-12 11:18:59 +0530

    Time: 1 ms (99.60%), Space: 41.7 MB (70.97%) - LeetHub

---
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -0,0 +1,22 @@
+class Solution {
+    int count = 0;
+
+    public void traverse(char theArray[], boolean used[]) {
+        count++; // count the number of sub-permutation
+        for(int i = 0; i < theArray.length; i++) {
+            //removing duplicates
+            if( used[i] || (i > 0 && theArray[i] == theArray[i - 1] && !used[i - 1]) ) 
+                continue;
+            used[i] = true;
+            traverse(theArray, used);
+            used[i] = false;
+        }
+    }
+
+    public int numTilePossibilities(String tiles) {
+        char theArray[] = tiles.toCharArray(); 
+        Arrays.sort(theArray); 
+        traverse(theArray, new boolean[theArray.length]);
+        return count - 1;
+    }
+}
