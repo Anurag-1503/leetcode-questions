b0469fb29d7f0b3ce81c92de1db422e74c2e644a
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-17 21:33:18 +0530

    Time: 57 ms (5.04%), Space: 50.8 MB (46.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,23 +1,28 @@
 class Solution {
-    public int longestCommonSubsequence(String s1, String s2) {
-		int[][] dp = new int[s1.length() + 1][s2.length() + 1];
+    int[][] memo;
+    public int longestCommonSubsequence(String text1, String text2) {
         
-        for(int i = 0 ; i <= s1.length();i++){
-            dp[i][0] = 0;
-        }
-        for(int j = 0 ; j <= s2.length(); j++){
-            dp[0][j] = 0;
-        }
-		for (int i = 1; i <= s1.length(); i++) {
-			for (int j = 1; j <= s2.length(); j++) {
-				if (s1.charAt(i-1) == s2.charAt(j-1)) {
-					dp[i][j] = 1 + dp[i - 1][j - 1];
-				} else {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-				}
-			}
-		}
-		return dp[s1.length()][s2.length()];
-	}
+        memo = new int[text1.length()][text2.length()];
+        for(int[] row : memo)
+            Arrays.fill(row , -1);
+        return helper(0,0,text1,text2);
+        
+    }
     
+    public int helper(int i , int j , String text1, String text2){
+        
+        if(i >= text1.length() || j >= text2.length())
+            return 0;
+        
+        if(memo[i][j] != -1)
+            return memo[i][j];
+        
+        int matched = 0;
+        if(text1.charAt(i) == text2.charAt(j))
+            matched = 1 + helper(i+1,j+1,text1,text2);
+        int unmatched = 0 + Math.max( helper(i,j+1,text1,text2) , helper(i+1,j,text1,text2));
+        
+        
+        return memo[i][j] =  Math.max(matched , unmatched);
+    }
 }
