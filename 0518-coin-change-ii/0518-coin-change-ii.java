ec4f4858eeb3c3e9cf6314462a4e2eb854be3e9c
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-17 11:20:54 +0530

    Time: 15 ms (18.92%), Space: 48.1 MB (22.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,29 +1,33 @@
 class Solution {
-    public int change(int T, int[] arr) {
-        int n = arr.length;
-        int dp[][]=new int[n][T+1];
     
-    
-    //Initializing base condition
-    for(int i=0;i<=T;i++){
-        if(i%arr[0]==0)
-            dp[0][i]=1;
-        // Else condition is automatically fulfilled,
-        // as dp array is initialized to zero
+    int[][] memo;
+    public int change(int amount, int[] coins) {
+        
+        memo = new int[coins.length][amount + 1];
+        for(int[] row : memo)
+            Arrays.fill(row , -1);
+        
+        return helper(0,coins,amount);
+        
     }
     
-    for(int ind=1; ind<n;ind++){
-        for(int target=0;target<=T;target++){
-            int notTaken = dp[ind-1][target];
-            
-            int taken = 0;
-            if(arr[ind]<=target)
-                taken = dp[ind][target-arr[ind]];
-                
-            dp[ind][target] = notTaken + taken;
-        }
-    }
-    
-    return dp[n-1][T];
+    public int helper(int index, int[] coins , int amount){
+        
+        if(amount == 0)
+            return 1;
+        
+        if(index >= coins.length || amount < 0)
+            return 0;
+        
+        if(memo[index][amount] != -1)
+            return memo[index][amount];
+        
+        
+        int take = (amount >= coins[index]) ? helper(index , coins , amount-coins[index]) : 0;
+        int nottake = helper(index + 1 , coins , amount);
+        
+        memo[index][amount] =  take + nottake;
+        
+        return memo[index][amount];
     }
 }
