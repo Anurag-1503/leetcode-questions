818f7f71de3fe1f657d9675f3a28184393933e9a
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-18 21:35:53 +0530

    Time: 3 ms (11.02%), Space: 48.9 MB (5.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,25 +1,46 @@
 class Solution {
+    
+    int[][] memo;
     public int maxProfit(int[] prices) {
         
-        int buydate = 0;
-        int selldate = 0;
-        int profit = 0;
-        
-        for(int i = 1 ; i < prices.length ; i++){
-            //if graph is going up , increment the selldate
-            if(prices[i] >= prices[i-1])
-                selldate++;
-            else
-            {
-                //if counter a drop in graph , then collect the profit and reset the buyandselldates on currentdate
-                profit += prices[selldate] - prices[buydate];
-                buydate = i;
-                selldate = i;
-            }
+        //2 as column because canBuy have only 2 values , 1 or 0
+        memo = new int[prices.length][2];
+        for(int[] row : memo){
+            Arrays.fill(row , -1);
         }
-        //there might exist one more transaction at last , so include it too
-        profit += prices[selldate] - prices[buydate];
+       //0 indicates that you have space to buy
+        //1 indicates that you are full and cant buy
+        return helper(0,prices,0);
+    }
+    
+    public int helper(int index , int[] arr , int canBuy)
+    {
+        if(index == arr.length)
+            return 0;
         
-        return profit;
+        if(memo[index][canBuy] != -1)
+            return memo[index][canBuy];
+            
+        int take = 0;
+        int nottake = 0;
+        //you can buy
+        if(canBuy == 0) {
+            //you can either buy or skip
+            //also make canBuy = 1 so that you can indicate that you are not allowed to buy in next days , you have to sell it first
+             take = -arr[index] + helper(index+1,arr,1);
+            //if you skip , you can buy on next day
+             nottake = 0 + helper(index+1,arr,0);
+        }
+        //you cant buy as you already have a stock to sell
+        else
+        {
+            //either you sell it or skip it
+            //change canBuy to 0 to show that you have sold the share and can buy on next day
+             take = arr[index] + helper(index+1,arr,0);
+            //or you skip it but cant buy on next day
+             nottake = 0 + helper(index+1,arr,1);
+        }
+        
+        return memo[index][canBuy] = Math.max(take, nottake);
     }
 }
