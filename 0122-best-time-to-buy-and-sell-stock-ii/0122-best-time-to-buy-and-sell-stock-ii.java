160b7e0dc6f931362f34e87c06a8c9193f90a207
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-18 20:51:28 +0530

    Time: 1 ms (93.55%), Space: 45.5 MB (83.78%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,21 +1,25 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
-        int currProfit = 0;
-        int totalProfit = 0;
-
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] > prices[i - 1]) {
-                currProfit += prices[i] - prices[i - 1];
-            } else {
-                totalProfit += currProfit;
-                currProfit = 0;
-                min = prices[i]; // Update min for the next potential buy point
+        
+        int buydate = 0;
+        int selldate = 0;
+        int profit = 0;
+        
+        for(int i = 1 ; i < prices.length ; i++){
+            //if graph is going up , increment the selldate
+            if(prices[i] >= prices[i-1])
+                selldate++;
+            else
+            {
+                //if counter a drop in graph , then collect the profit and reset the buyandselldates on currentdate
+                profit += prices[selldate] - prices[buydate];
+                buydate = i;
+                selldate = i;
             }
         }
-
-        totalProfit += currProfit; // Add any remaining profit
-
-        return totalProfit;
+        //there might exist one more transaction at last , so include it too
+        profit += prices[selldate] - prices[buydate];
+        
+        return profit;
     }
 }
