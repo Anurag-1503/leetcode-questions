0fef616e750c2a27950298504d8740852a1dcf5c
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-02-19 12:04:53 +0530

    Time: 389 ms (7.09%), Space: 78.7 MB (5.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,23 +1,37 @@
 class Solution {
+
+    int memo[][];
     public int lengthOfLIS(int[] nums) {
-        int n = nums.length;
-        int[][] dp = new int[n+1][n+1];
-     
         
-       for(int index = n-1; index>=0; index--){
-        for (int prev = index-1; prev >=-1; prev--){
-                
-                
-                int take = 0;
-                int nottake = 0 + dp[index+1][prev+1];
-                //we can take only if prev == -1 or it is greater than its prev index
-                if(prev == -1 || nums[index] > nums[prev])
-                    take = 1 + dp[index+1][index+1];
-            
-            dp[index][prev+1] = Math.max(take,nottake);
+        memo = new int[nums.length+1][nums.length+1];
+        for(int[] row : memo)
+            Arrays.fill(row , -1);
+        //0 is starting index and -1  is previous index, as nothing is previous to 0, we took it as -1
+        return helper(0,-1,nums);
         
-            }
+    }
+    
+    public int helper(int index , int prev , int[] nums) {
+        
+        //base condition for out of bound
+        if(index >= nums.length)
+            return 0;
+        
+        if(prev != -1 && memo[index][prev] != -1)
+            return memo[index][prev];
+        //we can take if we are at at first index , so prev will be -1 at first
+        //or we can take only if previous element was smaller than current element
+        int take = 0;
+        if(prev == -1 || nums[prev] < nums[index]){
+            //make the prev as current index for next recursive call
+            take = 1 + helper(index+1,index,nums);
         }
-     return dp[0][0];
+        
+        int skip = 0 + helper(index+1,prev,nums);
+        
+        if(prev != -1)
+            memo[index][prev] = Math.max(take , skip);
+        
+        return Math.max(take , skip);
     }
 }
