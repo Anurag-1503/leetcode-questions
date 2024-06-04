6ec0b1a7fa4e042ff75777b8a87c44fecf639bff
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-04 16:45:23 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (76.36%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.java
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -1,19 +1,14 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
         
-        List<Integer> arr = new ArrayList<>();
-        
-        for(int i = 0 ; i < accounts.length; i++){
-            int count = 0;
-            for(int j = 0 ; j < accounts[0].length ; j++){
-                
-                count += accounts[i][j];
+        int maxWealth = 0;
+        for(int i = 0 ; i < accounts.length ; i++) {
+            int max = 0;
+            for(int j = 0 ; j < accounts[0].length ; j++) {
+                max += accounts[i][j];
             }
-            
-            arr.add(count);
+            maxWealth = Math.max(maxWealth , max);
         }
-        
-        Collections.sort(arr);
-        return arr.get(arr.size()-1);
+        return maxWealth;
     }
 }
