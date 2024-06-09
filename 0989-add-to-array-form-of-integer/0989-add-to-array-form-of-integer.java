8bd1c9da0d644a2a5569bdd3122033b3a71812ae
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-09 10:22:08 +0530

    Time: 54 ms (34.56%), Space: 45.9 MB (17.33%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -1,22 +1,21 @@
 class Solution {
     public List<Integer> addToArrayForm(int[] num, int k) {
-        int n = num.length;
-        int carry = 0;
+        
         List<Integer> res = new ArrayList<>();
-
-        for(int i = n - 1; i >= 0 || k > 0 || carry > 0; i--) {
-            if (i >= 0) {
-                carry += num[i];
-            }
-            if (k > 0) {
-                carry += k % 10;
-                k /= 10;
-            }
-            res.add(carry % 10);
-            carry /= 10;
+        
+        for(int i = num.length -1 ; i >=0 ; i--) {
+            //add k to last index , and place only unit place digit in that last index , rest take it as k
+            res.add(0,(num[i]+k)%10);
+            //update k
+            k = (num[i]+k)/10;
         }
-
-        Collections.reverse(res);
+        //check if k is still remaining , if yes , then add it to 0th index
+        while(k>0) {
+            res.add(0,k%10);
+            k=k/10;
+        }
+            
         return res;
+        
     }
 }
