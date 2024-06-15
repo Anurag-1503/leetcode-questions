daa7a7743c82066a77adc3e48a49cdbf8bff23ed
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-15 08:33:09 +0530

    Time: 2 ms (93.22%), Space: 45 MB (80.70%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.java
@@ -1,37 +1,35 @@
 class Solution {
     public List<Integer> luckyNumbers (int[][] matrix) {
         
-        //first find the minimum element of each row and store it in a separate list
-        List<Integer> minimum = new ArrayList<>();
-        
-        for(int i = 0 ; i < matrix.length; i++) {
+       List<Integer> res = new ArrayList<>();
+
+        // Iterate through each row
+        for (int i = 0; i < matrix.length; i++) {
+            // Find the minimum element in the current row and its column index
             int min = Integer.MAX_VALUE;
-            for(int j = 0 ; j < matrix[0].length ; j++) {
-                min = Math.min(min , matrix[i][j]);
-            }
-            minimum.add(min);
-        }
-        
-        //Now find the Maximum element of each column and store it in a separate list
-        List<Integer> maximum = new ArrayList<>();
-        
-        for(int j = 0 ; j < matrix[0].length; j++) {
-            int max = Integer.MIN_VALUE;
-            for(int i = 0 ; i < matrix.length ; i++) {
-                max = Math.max(max , matrix[i][j]);
-            }
-            maximum.add(max);
-        }
-        
-        //now find the common element in both these lists and add it in result list
-        List<Integer> res = new ArrayList<>();
-        for(int val1 : minimum) {
-            for(int val2 : maximum) {
-                if(val1 == val2) {
-                    res.add(val1);
+            int minIndex = -1;
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < min) {
+                    min = matrix[i][j];
+                    minIndex = j;
                 }
             }
+
+            // Check if the found minimum element is the maximum in its column
+            boolean isMaxInColumn = true;
+            for (int k = 0; k < matrix.length; k++) {
+                if (matrix[k][minIndex] > min) {
+                    isMaxInColumn = false;
+                    break;
+                }
+            }
+
+            // If the minimum element in the row is also the maximum in its column, it's a lucky number
+            if (isMaxInColumn) {
+                res.add(min);
+            }
         }
+
         return res;
         
     }
