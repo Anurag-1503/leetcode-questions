09913e7b596422ab9094242f9297dcd65d324339
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-23 20:57:27 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (79.85%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.java
@@ -1,21 +1,27 @@
 class Solution {
     public char findKthBit(int n, int k) {
-        //Base Condition
-        if(n == 1){
-            return '0';
-        }
         
-        int numberOfColumns = (int)Math.pow(2,n) - 1;
-        int mid = numberOfColumns/2;
-                
+        //hint : try to deduce the cases of kth position given
+        
+        //Base case
+        if(n == 1)
+            return '0';
+        
+        //length of the binary string for n
+        int length = (int) Math.pow(2 , n) - 1;
+        int mid = length/2;
+        
+        //3 cases of kth bit
+        
         if(k <= mid)
             return findKthBit(n-1,k);
         else if (k == mid + 1)//Since we are adding that extra "1" in each new row
             return '1';
         else {
-           k = numberOfColumns - k + 1;
+           k = length - k + 1;
            char c = findKthBit(n-1,k);
            return c == '0' ? '1' : '0';
         }
+        
     }
 }
