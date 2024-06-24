f992e870887fcd06006ae76df8a363aae3cc135c
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-24 09:55:23 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (36.32%) - LeetHub

--- 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -1,20 +1,22 @@
 class Solution {
-     public int kthGrammar(int n, double k) {
-        // base condition
-        if(n==1 && k==1){
-            return 0;
-        }
-        // mid is size of 1st half of grammar generated
-        // 1st half elements of row n would always equal to that of row (n-1)
-         
-        double mid = Math.pow(2,n-1) / 2; 
-      
-        if(k<=mid)
-            return kthGrammar(n-1, k);    
-        else{
-            int t = kthGrammar(n-1, k-mid);
-            return t^1; // XOR // invert
-        }
+    public int kthGrammar(int n, int k) {
         
+        //Base case
+        if(n == 1)
+            return 0;
+        
+        int length = (int)Math.pow(2,n-1);
+        int mid = length/2;
+        
+        //cases for Kth bit position
+        if(k <= mid)
+            return kthGrammar(n-1,k);
+        else
+        {
+            //if k > mid
+            k = k - mid;
+            int bit = kthGrammar(n-1,k);
+            return bit == 0 ? 1 : 0;
+        }
     }
 }
