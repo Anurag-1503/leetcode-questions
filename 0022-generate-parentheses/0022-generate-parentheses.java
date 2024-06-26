5d242bc4ebef9a212206afd279e3a6abbfc648d9
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-26 10:02:44 +0530

    Time: 14 ms (5.14%), Space: 45.1 MB (5.38%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -1,22 +1,35 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
         List<String> result = new ArrayList<>();
-        helper(result , "" , 0 , 0 , n);
+        generate("", n, 0, result);
         return result;
-        
     }
-    
-    public void helper(List<String> result , String temp , int open , int close , int max){
-        
-        if(temp.length() == max*2)
-        {
-            result.add(temp);
+
+    private boolean isValid(String str) {
+        int sum = 0;
+        for (char ch : str.toCharArray()) {
+            if (ch == '(')
+                sum++;
+            else
+                sum--;
+            if (sum < 0)
+                return false;
+        }
+        return sum == 0;
+    }
+
+    private void generate(String curr, int n, int length, List<String> result) {
+        if (length == 2 * n) {
+            if (isValid(curr))
+                result.add(curr);
             return;
         }
-        
-        if(open < max)
-            helper(result , temp + "(" , open + 1, close , max);
-        if(close < open)
-            helper(result , temp + ")" , open , close + 1, max);
+
+        curr += '(';
+        generate(curr, n, length + 1, result);
+        curr = curr.substring(0, curr.length() - 1);
+
+        curr += ')';
+        generate(curr, n, length + 1, result);
     }
 }
