9eb47cfb1bdcd358d5e998256e0db2ed8a19e063
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-06-27 10:56:39 +0530

    Time: 1 ms (52.37%), Space: 42.2 MB (92.61%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,26 +1,23 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> res= new ArrayList<>();
         List<Integer> ds = new ArrayList<>();
-        helper(0,nums,result,ds);
-        return result;
+        
+        helper(0,nums,res,ds);
+        return res;
     }
     
-    public void helper(int index , int[] nums ,List<List<Integer>> result ,List<Integer> ds){
+    public void helper(int index , int[] nums , List<List<Integer>> res , List<Integer> ds) {
         
-        //base condition
-        if(index >= nums.length)
-        {
-            //if you directly add ds , any change in later backtracking will affect the ds added in result 
-            //thats why you need to add a copy of ds
-            result.add(new ArrayList<>(ds));
+        if(index > nums.length-1) {
+            res.add(new ArrayList<>(ds));
             return;
         }
         
         ds.add(nums[index]);
-        helper(index + 1, nums , result , ds);
+        helper(index+1,nums,res,ds);
         ds.remove(ds.size()-1);
-        helper(index + 1 , nums , result ,ds);
+        helper(index+1,nums,res,ds);
     }
 }
