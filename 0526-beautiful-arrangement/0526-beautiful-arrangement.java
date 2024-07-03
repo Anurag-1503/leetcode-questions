6c90297a5e1ec1f466f21ac59010daa4baaa0508
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-07-03 10:28:09 +0530

    Time: 43 ms (43.19%), Space: 40.3 MB (55.45%) - LeetHub

--- 0526-beautiful-arrangement/0526-beautiful-arrangement.java
+++ 0526-beautiful-arrangement/0526-beautiful-arrangement.java
@@ -1,29 +1,38 @@
 class Solution {
+    public int countArrangement(int n) {
+        // Initialize the count of beautiful arrangements
+        int[] count = new int[1];
+        
+        // Create an array to track which numbers have been used in the permutation
+        boolean[] used = new boolean[n + 1];
+        
+        // Start the backtracking process from position 1
+        backtrack(n, 1, used, count);
+        
+        // Return the total count of beautiful arrangements
+        return count[0];
+    }
     
-    int count = 0;
-
-        public int countArrangement(int n) {
-            int[] nums = new int[n];
-            for (int i = 0; i < n; i++)
-                nums[i] = i + 1;
-            permute(nums, 0);
-            return count;
+    private void backtrack(int n, int pos, boolean[] used, int[] count) {
+        // If we've reached the end of the permutation, increment the count
+        if (pos > n) {
+            count[0]++;
+            return;
         }
-
-        private void permute(int[] nums, int k) {
-            if (k == nums.length)
-                count++;
-            for (int i = k; i < nums.length; i++) {
-                swap(nums, i, k);
-                if (nums[k] % (k + 1) == 0 || (k + 1) % nums[k] == 0)
-                    permute(nums, k + 1);
-                swap(nums, i, k);
+        
+        // Try placing each number from 1 to n at the current position
+        for (int i = 1; i <= n; i++) {
+            // Check if the number i can be placed at position pos
+            if (!used[i] && (i % pos == 0 || pos % i == 0)) {
+                // Mark the number i as used
+                used[i] = true;
+                
+                // Recurse to place the next number
+                backtrack(n, pos + 1, used, count);
+                
+                // Unmark the number i to backtrack
+                used[i] = false;
             }
         }
-
-        private void swap(int[] nums, int i, int j) {
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+    }
 }
