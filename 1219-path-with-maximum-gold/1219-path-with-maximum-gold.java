6b6430b9f51a0a29828af97124c0ba9b2c73dc9d
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-07-05 10:39:02 +0530

    Time: 64 ms (67.93%), Space: 41.3 MB (31.24%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.java
@@ -1,41 +1,43 @@
-class Solution{
-    
+class Solution {
+    int max = 0;
+
     public int getMaximumGold(int[][] grid) {
-    
-    int maxGold = 0;
-    
-    for (int i = 0; i < grid.length; i++) {
-        for (int j = 0; j < grid[0].length; j++) {
-            if (grid[i][j] != 0) {
-                maxGold = Math.max(maxGold, helper(grid, i, j));
+        int m = grid.length;
+        int n = grid[0].length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] != 0) {
+                    travelAndCollectGold(i, j, grid, 0);
+                }
             }
         }
-    }
-    return maxGold;
-}
 
-private int helper(int[][] grid, int x, int y) {
-    //if out of bounds or no gold , return
-    if (x < 0 || grid.length - 1 < x || y < 0 || grid[0].length - 1 < y || grid[x][y] == 0) {
-        return 0;
+        return max;
     }
-    //store copy of current position's gold
-    int temp = grid[x][y];
-    
-    //make the current position 0 to make sure its visited
-    grid[x][y] = 0;
-    //travel in all directions
-    int up = helper(grid, x - 1, y);
-    int down = helper(grid, x + 1, y);
-    int left = helper(grid, x, y - 1);
-    int right = helper(grid, x, y + 1);
-    
-    //restore the original gold to current index(backtracking)
-    grid[x][y] = temp;
-    
-    //return the one maximum value of gold from all directions
-    return Math.max(Math.max(Math.max(left, right), up), down) + temp;
-}
 
-    
+    public void travelAndCollectGold(int i, int j, int[][] grid, int currentSum) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        // Base case: if out of bounds or cell has no gold
+        if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0) {
+            max = Math.max(max, currentSum);
+            return;
+        }
+
+        int originalValue = grid[i][j];
+
+        // Mark the cell as visited
+        grid[i][j] = 0;
+
+        // Explore in four directions
+        travelAndCollectGold(i - 1, j, grid, currentSum + originalValue); // north
+        travelAndCollectGold(i, j + 1, grid, currentSum + originalValue); // east
+        travelAndCollectGold(i, j - 1, grid, currentSum + originalValue); // west
+        travelAndCollectGold(i + 1, j, grid, currentSum + originalValue); // south
+
+        // Restore the original value before backtracking
+        grid[i][j] = originalValue;
+    }
 }
