b1a2a863f2f8b94181dc7388e61f6e0f54d5e260
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-07-08 09:30:30 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (16.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,28 +1,27 @@
 class Solution {
-    int[] memo ;
+    
+    int[] memo;
     public int rob(int[] nums) {
         
         memo = new int[nums.length];
         Arrays.fill(memo , -1);
-        int n = nums.length - 1;
-        return helper(n,nums);
+        return helper(0,nums);
         
     }
     
-    public int helper(int index , int[] nums){
-        if(index == 0)
-            return nums[index];
+    public int helper(int index , int[] nums) {
         
-        if(index < 0)
+        if(index >= nums.length)
             return 0;
         
         if(memo[index] != -1)
             return memo[index];
         
-        int pick = nums[index] + helper(index - 2 , nums);
-        int notpick = 0 + helper(index - 1 , nums);
+        //steal condition
+        int steal = nums[index] + helper(index+2 , nums);
+        int skip = helper(index+1 , nums);
         
-        memo[index] = Math.max(pick , notpick);
+        memo[index] = Math.max(steal , skip);
         
         return memo[index];
     }
