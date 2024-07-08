47950986ac412c926fa2445181088dc6d104d464
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-07-08 09:12:52 +0530

    Time: 351 ms (8.50%), Space: 45.8 MB (22.83%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,32 +1,30 @@
 class Solution {
+    
     int[] memo;
     public boolean canJump(int[] nums) {
         
         memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        return helper(0,nums);
+        Arrays.fill(memo , -1);
+        return helper(0 , nums);
+        
     }
     
-    public boolean helper(int index , int[] nums){
-        
+    public boolean helper(int index, int[] nums) {
         if(index == nums.length - 1)
             return true;
         
         if(memo[index] != -1)
             return (memo[index] == 1);
-        //calculate the maximum jump from current index
-        //we have to ensure that the maximum jump should be less than nums.length
-        int maxJump = Math.min(index + nums[index] , nums.length - 1);
         
-        
-        for(int position = index + 1 ; position <= maxJump ; position++){
-            if(helper(position , nums))
+        for(int i = 1 ; i <= nums[index] ; i++) {
+            if(helper(index+i , nums))
             {
-                memo[position] = 1; 
+                memo[index] = 1;
                 return true;
             }
                 
         }
+        
         memo[index] = 0;
         return false;
     }
