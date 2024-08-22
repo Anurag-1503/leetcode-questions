b7dc4151d0df79c762901bae149a557e2155a97f
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-08-22 20:25:00 +0530

    Time: 67 ms (5.29%), Space: 44.9 MB (12.05%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
+        //in this approach we can make a new StringBuilder same as Strint t, and compare it with s
+        //for each character , if found in both , then delete from StringBuilder , else return false;
         if(s.length() != t.length())
             return false;
-        
-        char[] arr1 = s.toCharArray();
-        char[] arr2 = t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        
-        for(int i = 0 ; i < arr1.length ; i++){
-            if(arr1[i] != arr2[i])
+            
+        StringBuilder sb = new StringBuilder(t);
+        for(int i = 0 ; i < s.length() ; i++){
+            char curr = s.charAt(i);
+            int index = sb.indexOf(String.valueOf(curr)); //finds the index of current char of s in sb
+            
+            //if not found , return false;
+            //if found , then delete that matched char from sb
+            if(index == -1)
                 return false;
+            else
+                sb.deleteCharAt(index);
         }
-        
-        return true;
+       
+       return true;
         
     }
 }
