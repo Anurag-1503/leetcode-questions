3d5d8cb96a9036afd84c829c7796b73adb5198d0
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-08-22 20:40:21 +0530

    Time: 6 ms (42.20%), Space: 43.1 MB (75.12%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,25 +1,32 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         
-        //in this approach we can make a new StringBuilder same as Strint t, and compare it with s
-        //for each character , if found in both , then delete from StringBuilder , else return false;
-        if(s.length() != t.length())
+         
+        //O(n)
+        
+        //Increment the count for each character in s.
+        //Decrement the count for each character in t.
+        //Check if all counts are zero:
+        //If all counts are zero after processing both strings, then s and t are anagrams.
+        
+         if(s.length() != t.length())
             return false;
             
-        StringBuilder sb = new StringBuilder(t);
-        for(int i = 0 ; i < s.length() ; i++){
-            char curr = s.charAt(i);
-            int index = sb.indexOf(String.valueOf(curr)); //finds the index of current char of s in sb
-            
-            //if not found , return false;
-            //if found , then delete that matched char from sb
-            if(index == -1)
-                return false;
-            else
-                sb.deleteCharAt(index);
+        int[] table = new int[26];
+        
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            table[s.charAt(i) - 'a']++;
+            table[t.charAt(i) - 'a']--;
         }
-       
-       return true;
+        
+        //now check if every element is 0 , if no then it was an anagram(i.e. return false)
+        for(int val : table){
+            if(val != 0)
+                return false;
+        }
+        
+        return true;
         
     }
 }
