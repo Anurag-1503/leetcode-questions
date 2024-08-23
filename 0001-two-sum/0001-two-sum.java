af1a936b1ac546b717c5d75d84c06ae318eaf601
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-08-23 20:20:55 +0530

    Time: 2 ms (98.15%), Space: 44.9 MB (29.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,18 +1,22 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         
-         //O(n^2)
-        //BRUTE FORCE SOLUTION : iterate through all elements in array to find the pair
-        
-        for(int i = 0 ; i < nums.length ; i++){
-            for(int j = i+1 ; j < nums.length ; j++){
-                
-                if(nums[i]+nums[j] == target)
-                    return new int[] {i,j};
-            }
-        }
-        //if pair not found , then return {-1,-1} array
-        return new int[] {-1,-1};
+        //o(n)
+        //optimized solution : use a map to store element with its index , and concurrently check if 
+        //complement exists in map, if yes then pair found (return it) , else return {-1,-1}
+        Map<Integer , Integer> map = new HashMap<>();
+       
+       for(int i = 0 ; i < nums.length ; i++){
+           
+           int complement = target - nums[i];
+           
+           if(map.containsKey(complement))
+               return new int[] {i , map.get(complement)};
+           
+           map.put(nums[i] , i);
+       }
+       
+       return new int[] {-1,-1};
         
     }
 }
