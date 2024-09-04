3ac0ed18bf0f425cc64af501c0e047fb4fdf8ce0
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-09-04 10:27:45 +0530

    Time: 2 ms (87.16%), Space: 55 MB (96.24%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,27 +1,51 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
+        
         int n = nums.length;
-      
+        int[] ans = new int[n];
         
-        int[] answer = new int[n];
+        //to calculate the overall product of array
+        int max_product = 1;
+        for(int var : nums)
+            max_product *= var;
         
-          answer[0]=1;
-        
-        for( int i =1 ; i< n ; i++)
-        {
-            answer[i]=nums[i-1]*answer[i-1];
+        //to calculate the overall product of array except zero elements
+        int max_product_wz = 1;
+        for(int var : nums){
+            if(var != 0)
+               max_product_wz *= var;
         }
         
-        int V = 1;
-        for( int i =n-1 ; i>=0 ; i--)
-        {
-            answer[i] = V * answer[i];
-            V = V * nums[i];
+        //to calculate the number of zeroes
+        int count = 0;
+        for(int var : nums){
+            if(var == 0)
+                count++;
         }
         
-       
+        for(int i = 0 ; i < n ; i++) {
+            //if the curr element is nonzero 
+            if(nums[i] != 0) {
+                //check if there exists a zero in array , if yes then store 0
+                if(count > 0)
+                    ans[i] = 0;
+                //if not then divide
+                else
+                    ans[i] = max_product_wz/nums[i];
+            }
+            else {
+                //if curr element is zero
+                //check if there exists another zero in array except curr , if yes then store 0
+                if(count > 1)
+                    ans[i] = 0;
+                //if not then store the max product without zero
+                else
+                    ans[i] = max_product_wz;  
+            }
+        }
         
         
-        return answer;
+        
+        return ans;
     }
 }
