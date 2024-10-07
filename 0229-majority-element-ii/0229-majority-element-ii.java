afb0ab30b0614f10362da443b325f518904e8810
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-10-07 12:47:40 +0530

    Time: 11 ms (45.15%), Space: 47.9 MB (49.23%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,54 +1,21 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
-         List<Integer> result = new ArrayList<>();
-
-        int count1 = 0, count2 = 0;
-        int element1 = 0, element2 = 0;
-
-        // Voting process to find potential majority elements
-        for (int num : nums) {
-            if (num == element1) {
-                count1++;
-            } else if (num == element2) {
-                count2++;
-            } else if (count1 == 0) {
-                element1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                element2 = num;
-                count2 = 1;
-            } else {
-                count1--;
-                count2--;
-            }
-        }
-
-        // Reset counts for the second pass
-        count1 = 0;
-        count2 = 0;
-
-        // Count occurrences of potential majority elements
-        for (int num : nums) {
-            if (num == element1) {
-                count1++;
-            } else if (num == element2) {
-                count2++;
-            }
-        }
-
-        // Check if potential majority elements meet the condition and add to result
-        if (count1 > nums.length / 3) {
-            result.add(element1);
-        }
-        if (count2 > nums.length / 3) {
-            result.add(element2);
-        }
-
-        return result;
+        Map<Integer , Integer> map = new HashMap<>();
+        List<Integer> res = new ArrayList<>();
+        
+        int target = nums.length/3;
         
         
+        for(int val : nums)
+            map.put(val , map.getOrDefault(val , 0) + 1);
+
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()) {
+            if(entry.getValue() > target)
+                res.add(entry.getKey());
+        }
         
+        return res;
         
     }
 }
