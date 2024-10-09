6a0372e45f3b190a796389087c47ffdfb9a66c00
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-10-09 09:33:58 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (39.86%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,49 +1,54 @@
 class Solution {
     public void nextPermutation(int[] nums) {
+        
         int n = nums.length;
-        int index = -1;
-
-        // Find the breakpoint where nums[i] < nums[i+1]
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        int pivot = -1;
+        
+        //first find pivot (where nums[i]> nums[i-1])
+        for(int i = n-1 ; i > 0 ; i--) {
+            if(nums[i-1] < nums[i]) {
+                pivot = i-1;
                 break;
             }
         }
-
-        // If breakpoint doesn't exist, it means the array is in reverse-sorted order
-        // and the next permutation of it is the reverse of itself
-        if (index == -1) {
-            reverse(0, n - 1, nums);
+        
+        //if pivot is still -1 , it means the array is in reverse-sorted order
+        //and the next permutation of it is the reverse of itself
+        
+        if(pivot == -1){
+            reverse(nums , 0 , n-1);
             return;
         }
-
-        // Find the smallest element greater than nums[index]
-        for (int i = n - 1; i >= 0; i--) {
-            if (nums[i] > nums[index]) {
-                // Swap the elements
-                int temp = nums[i];
-                nums[i] = nums[index];
-                nums[index] = temp;
-
-                // Break after the swap
+        
+        //else , find just greater element on the right to pivot , and swap them
+        for(int i = n-1 ; i > pivot; i--) {
+            if(nums[i] > nums[pivot]) {
+                
+                //swap
+                int temp = nums[pivot];
+                nums[pivot] = nums[i];
+                nums[i] = temp;
+                
                 break;
             }
         }
-
-        // Sort the rest of the elements from index+1 to end
-        reverse(index + 1, n - 1, nums);
+        
+        //reverse the elements next to pivot
+        reverse(nums , pivot+1 , n-1);
+        
     }
-
-    public void reverse(int i, int j, int[] nums) {
-        int start = i;
-        int end = j;
-        while (start < end) {
-            int temp = nums[start];
-            nums[start] = nums[end];
-            nums[end] = temp;
-            start++;
-            end--;
+    
+    public void reverse(int[] nums , int l , int r) {
+        
+        int left = l;
+        int right = r;
+        
+        while(left <= right) {
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left++;
+            right--;
         }
     }
 }
