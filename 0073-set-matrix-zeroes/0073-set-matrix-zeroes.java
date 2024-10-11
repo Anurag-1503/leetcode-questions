ae19fb626f780c68fbf68e36eda030665c6ab74f
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-10-11 10:51:01 +0530

    Time: 1 ms (78.71%), Space: 45.4 MB (82.93%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,28 +3,33 @@ class Solution {
         
         int r = matrix.length;
         int c = matrix[0].length;
+        int[][] matrix2 = new int[r][c];
         
-        List<Integer> row = new ArrayList<>();
-        List<Integer> col = new ArrayList<>();
+        //copying matrix to a new matrix(2)
+        for(int i = 0 ; i < r ; i++) {
+            for(int j = 0 ; j < c ; j++) {
+                matrix2[i][j] = matrix[i][j];
+            }
+        }
         
-        for(int i = 0; i < r; i++){
-            for(int j = 0; j < c ; j++){
-                if(matrix[i][j] == 0){
-                    row.add(i);
-                    col.add(j);
+        //traverse in original matrix and set rows and cols zero of matrix2 if encounters a zero in original matrix
+        for(int i = 0 ; i < r ; i++) {
+            for(int j = 0 ; j < c ; j++) {
+                if(matrix[i][j] == 0) {
+                    //set cols to zero
+                    for(int k = 0 ; k < c ; k++)
+                        matrix2[i][k] = 0;
+                    //set rows to zero
+                    for(int k = 0 ; k < r ; k++)
+                        matrix2[k][j] = 0;
                 }
             }
         }
         
-        for(int i = 0 ; i < row.size() ; i++){
-            for(int j = 0 ; j < c; j++){
-                matrix[row.get(i)][j] = 0;
-            }
-        }
-        
-        for(int i = 0 ; i < col.size() ; i++){
-            for(int j = 0 ; j < r; j++){
-                matrix[j][col.get(i)] = 0;
+        //now copy all elements from matrix2 to original matrix
+        for(int i = 0 ; i < r ; i++) {
+            for(int j = 0 ; j < c ; j++) {
+                matrix[i][j] = matrix2[i][j];
             }
         }
         
