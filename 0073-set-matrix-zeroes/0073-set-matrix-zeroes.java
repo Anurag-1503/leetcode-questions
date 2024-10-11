71cf716c7054f3edee68b05310ff46279386674b
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-10-11 10:56:45 +0530

    Time: 1 ms (78.71%), Space: 45.7 MB (31.77%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -3,33 +3,26 @@ class Solution {
         
         int r = matrix.length;
         int c = matrix[0].length;
-        int[][] matrix2 = new int[r][c];
+        int[] rowArray = new int[r];
+        int[] colArray = new int[c];
         
-        //copying matrix to a new matrix(2)
-        for(int i = 0 ; i < r ; i++) {
-            for(int j = 0 ; j < c ; j++) {
-                matrix2[i][j] = matrix[i][j];
-            }
-        }
+        Arrays.fill(rowArray , 1);
+        Arrays.fill(colArray , 1);
         
-        //traverse in original matrix and set rows and cols zero of matrix2 if encounters a zero in original matrix
         for(int i = 0 ; i < r ; i++) {
             for(int j = 0 ; j < c ; j++) {
                 if(matrix[i][j] == 0) {
-                    //set cols to zero
-                    for(int k = 0 ; k < c ; k++)
-                        matrix2[i][k] = 0;
-                    //set rows to zero
-                    for(int k = 0 ; k < r ; k++)
-                        matrix2[k][j] = 0;
+                    rowArray[i] = 0;
+                    colArray[j] = 0;
                 }
             }
         }
         
-        //now copy all elements from matrix2 to original matrix
         for(int i = 0 ; i < r ; i++) {
             for(int j = 0 ; j < c ; j++) {
-                matrix[i][j] = matrix2[i][j];
+                if(rowArray[i] == 0 || colArray[j] == 0) {
+                    matrix[i][j] = 0;
+                }
             }
         }
         
