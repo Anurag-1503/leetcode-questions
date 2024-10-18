e20e2947361da3486e9a65a742a4fdf9039e3b6b
Anurag_Gaur15 <101164004+Anurag-1503@users.noreply.github.com>
2024-10-18 09:27:22 +0530

    Time: 5 ms (90.78%), Space: 45.1 MB (93.94%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.java
+++ 1314-matrix-block-sum/1314-matrix-block-sum.java
@@ -5,6 +5,23 @@ class Solution {
         int cols = mat[0].length;
         int ans[][] = new int[rows][cols];
         
+        int psum[][] = mat;
+        
+        //cumulative sum of row elements
+        for(int i = 0 ; i < rows ; i++) {
+            for(int j = 1 ; j < cols ; j++) {
+                psum[i][j] += psum[i][j-1];
+            }
+        }
+        //cumulative sum of column elements
+        for(int j = 0 ; j < cols ; j++) {
+            for(int i = 1 ; i < rows ; i++) {
+                psum[i][j] += psum[i-1][j];
+            }
+        }
+        
+        //now we have our prefix sum array ready
+        
         for(int i = 0 ; i < rows ; i++) {
             for(int j = 0 ; j < cols ; j++) {
                 int r1 = Math.max(0 , i-k);
@@ -12,15 +29,25 @@ class Solution {
                 int r2 = Math.min(rows-1 , i+k);
                 int c2 = Math.min(cols-1 , j+k);
                 
-                for(int a = r1 ; a <= r2 ; a++) {
-                    for(int b = c1 ; b <= c2 ; b++) {
-                        ans[i][j] += mat[a][b];
-                    }
-                }
+                ans[i][j] = sumRegion(psum , r1 , c1 , r2 , c2);
             }
         }
         
         return ans;
+        
+    }
+    
+    public int sumRegion(int[][] psum , int row1, int col1, int row2, int col2) {
+        
+        int res = psum[row2][col2];
+        if(row1 > 0)
+            res -= psum[row1-1][col2];
+        if(col1 > 0)
+            res -= psum[row2][col1-1];
+        if(row1 > 0 && col1 > 0)
+            res += psum[row1-1][col1-1];
+        
+        return res;
         
     }
 }
